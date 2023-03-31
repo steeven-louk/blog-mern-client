e36df0172a00dd6693bc5189382e746bc8ecc29a
steeven-louk <steevenlouk.sl@gmail.com>
2023-03-31 18:32:00 +0200

    mise en place de la navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+import Homepage from './pages/Homepage';
+import Navbar from './components/navbar/Navbar'
+import PostDetail from './pages/PostDetail';
+import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-     
+    <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Homepage/>} />
+        <Route path='/write' exact element={<Write/>} />
+        <Route path='/post/:postId' exact element={ <PostDetail/>} />
+        <Route path="/settings" element={<Settings/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={ <Register/>} />
+      </Routes>
     </div>
   );
 }
